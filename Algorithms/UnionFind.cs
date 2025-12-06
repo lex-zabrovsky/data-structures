2028f84d96c3ab7e5a7ef84c9a38185b495235ae
Alexander Zabrovsky <lex.zabrovsky@gmail.com>
2025-12-06 04:57:54 +0000

    feat: Algorithms project added

---
+++ Algorithms/UnionFind.cs
@@ -0,0 +1,11 @@
+namespace Algorithms
+{
+  public class QuickFindUF
+  {
+    public void TestBuild()
+    {
+      Console.WriteLine(
+        $"{System.Reflection.MethodBase.GetCurrentMethod().Name.ToString()} Impl");
+    }
+  }
+}
